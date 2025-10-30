5d0ee40ffcf8c561bf97a30c18b03893be4d2001
7minutech <elijohnson1107@gmail.com>
2025-10-30 12:45:44 -0500

    feat: mastermind jsx boilerplate

---
+++ public/scripts/Mastermind.jsx
@@ -0,0 +1,41 @@
+    const codeLength = 4;
+    
+    const codePegs =  [
+        { name: "Green", color: "\u{1F7E2}" },
+        { name: "Blue", color: "\u{1F535}" },
+        { name: "Red", color: "\u{1F534}" },
+        { name: "Yellow", color: "\u{1F7E1}" },
+        { name: "Brown", color: "\u{1F7E4}" },
+        { name: "Orange", color: "\u{1F7E0}" }
+    ];
+
+    const codePegNames = [
+        "Green",
+        "Blue",
+        "Red",
+        "Yellow",
+        "Brown",
+        "Orange"
+    ];
+
+    const keyPegBlack = { name: "Black", color: "\u{26AB}" };
+    const keyPegWhite = { name: "White", color: "\u{26AA}" };
+
+    class MasterMind extends React.Component {
+        constructor(props) {
+            super(props)
+            this.state = {}
+        }
+
+        onClick = () => {
+
+        }
+
+        handlePegSelectorChange = () => {
+
+        }
+
+        render() {
+            
+        }
+    }
