002016af9c61be934153d5ea027d547d60dc2393
7minutech <elijohnson1107@gmail.com>
2025-10-30 12:11:23 -0500

    feat: server & npm init

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+export { app }
