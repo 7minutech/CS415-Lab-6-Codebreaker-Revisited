873340afc47e7444a98e26135cef73b83cb5bf86
7minutech <elijohnson1107@gmail.com>
2025-10-30 13:09:45 -0500

    feat: constructor

--- public/scripts/Mastermind.jsx
+++ public/scripts/Mastermind.jsx
@@ -24,7 +24,32 @@
     class MasterMind extends React.Component {
         constructor(props) {
             super(props)
-            this.state = {}
+            this.state = {
+                slots: [], guess_count: 0, white_pegs: 0, black_pegs: 0, code: [], code_counts: {},
+                game_over: false, guesses: {slot0: null, slot1: null, slot2: null, slot3: null}
+            }
+            // client code 
+            // this.buildPegSelectors();
+            // this.setSlots();
+            this.generate_code();
+            this.generate_code_counts();
+            console.log(this.code);
+        }
+
+        generateCode() {
+            for (let i = 0; i < codeLength; i++){
+                let random_index = [(Math.floor(Math.random() * codePegNames.length))];
+                this.state.code.push(codePegNames[random_index]);
+            }
+        }
+
+        generateCodeCounts() {
+            this.state.code.forEach((color) => {
+            if (!(color in this.state.code_counts)){
+                this.state.code_counts[color] = 0;
+            }
+                this.state.code_counts[color]++;
+            });
         }
 
         onClick = () => {
